965a1cb740a872968f0c75b9b07caa6be5f037d9
august95 <ingvar.wedervang@gmail.com>
2024-08-09 21:38:29 +0200

    global header file for brazer_compiler created. Contain static functions for error messages and logging

---
+++ braze_compiler/braze_compiler.h
@@ -0,0 +1,7 @@
+//global header file
+#include <iostream>
+
+static void cerror(const char* error) { std::cout << error << "\n"; }
+static void cwarning(const char* warning) { std::cout << warning << "\n"; }
+static void clog(const char* log) { std::cout << log << "\n"; }
+
