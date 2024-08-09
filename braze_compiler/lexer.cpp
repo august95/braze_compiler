e3085b93fef8176ba73b8e34dcf37d084c988000
august95 <ingvar.wedervang@gmail.com>
2024-08-09 20:08:48 +0200

    created empty functions for peeking, getting next and pushing char

--- braze_compiler/lexer.cpp
+++ braze_compiler/lexer.cpp
@@ -4,3 +4,18 @@ lexer::lexer()
 {
 
 }
+
+char lexer::peekChar()
+{
+	return '0';
+}
+
+char lexer::nextChar()
+{
+	return '0';
+}
+
+void lexer::pushChar()
+{
+
+}
--- braze_compiler/lexer.h
+++ braze_compiler/lexer.h
@@ -1,9 +1,17 @@
 #pragma once
+//lexing source code to create tokens
+
 class lexer
 {
 public:
 	lexer();
 
+
+private:
+
+	char peekChar();
+	char nextChar();
+	void pushChar();
 
 };
 
