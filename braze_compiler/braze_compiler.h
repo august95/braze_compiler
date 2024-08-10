799d3a88cd746680525d1659d2f96cd1b1118eec
august95 <ingvar.wedervang@gmail.com>
2024-08-10 11:01:29 +0200

    improved error warning and log prints

--- braze_compiler/braze_compiler.h
+++ braze_compiler/braze_compiler.h
@@ -1,7 +1,7 @@
 //global header file
 #include <iostream>
 
-static void cerror(const char* error) { std::cout << error << "\n"; }
-static void cwarning(const char* warning) { std::cout << warning << "\n"; }
-static void clog(const char* log) { std::cout << log << "\n"; }
+static void cerror(const char* error) { std::cout << "Compiler Error: " << error << "\n"; }
+static void cwarning(const char* warning) { std::cout << "Compiler Warning: " << warning << "\n"; }
+static void clog(const char* log) { std::cout << "Log: " << log << "\n"; }
 
