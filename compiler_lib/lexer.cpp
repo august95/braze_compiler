a5e3b80f61156b4d37b63698145c28fe95dace61
Ingvar August Wedervang <55133964+august95@users.noreply.github.com>
2024-08-11 22:30:06 +0200

    support for lexing decimal, binary and hexical numbers created

--- compiler_lib/lexer.cpp
+++ compiler_lib/lexer.cpp
@@ -2,7 +2,19 @@
 #include "lexer.h"
 #include "braze_compiler.h"
 #include <iostream>
+#include <assert.h>
 
+#define CASE_NUMERIC \
+    case '0':        \
+    case '1':        \
+    case '2':        \
+    case '3':        \
+    case '4':        \
+    case '5':        \
+    case '6':        \
+    case '7':        \
+    case '8':        \
+    case '9'
 
 lexer::lexer() :
 m_filename(),
@@ -16,6 +28,44 @@ lexer::~lexer()
 {
 	m_file.close();
 }
+
+
+char lexer::peekChar()
+{
+	if (m_file)
+	{
+		return m_file.peek();
+	}
+	cerror("could not open test.c");
+	return 'x';
+}
+
+char lexer::nextChar()
+{
+	if (m_file)
+	{
+		char c = m_file.get();
+		m_file_position.incrementCol();
+		if (c == '\n')
+		{
+			m_file_position.incrementLine();
+		}
+		return c;
+	}
+
+	cerror("could not open test.c");
+	return 'x';
+}
+
+void lexer::pushChar(char ch)
+{
+	if (!m_file)
+		return;
+	m_file.putback(ch);
+	cerror("LEX error: could not open test.c");
+}
+
+
 
 void lexer::initialize(std::string filename)
 {
@@ -45,49 +95,109 @@ void lexer::lexFile()
 	* A parser needs to access the tokens in the order they was created, queue or linked list might do the work
 	* 
 	*/
-	/*
-	token token = readNextToken();
+
+	std::shared_ptr< token > token = readNextToken();
 	while (token)
 	{
-		tokens->push(token);
+		tokens.push_back(token);
 		token = readNextToken();
 	}
-	*/
 
 	std::cout << "test";
 }
 
-char lexer::peekChar()
+
+void lexer::_assert_(bool condition)
 {
-	if (m_file)
-	{
-		return m_file.peek();
-	}
-	cerror("could not open test.c");
-	return 'x';
+	assert(condition);
 }
 
-char lexer::nextChar()
+std::shared_ptr< token > lexer::readNextToken()
 {
-	if (m_file)
+	std::shared_ptr < token > token(0);
+
+	char c = peekChar();
+	
+	switch (c)
 	{
-		char c = m_file.get();	
-		m_file_position.incrementCol();
-		if (c == '\n')
+	CASE_NUMERIC:
+		token = makeNumberToken();
+		break;
+	}
+
+	return token;
+}
+
+std::shared_ptr < token > lexer::makeNumberToken()
+{
+	std::shared_ptr < token > token(0);
+	if (peekChar() == '0')
+	{
+		char c = peekChar();
+		nextChar();
+
+		//hex number 0xFA3
+		if (peekChar() == 'x') 
 		{
-			m_file_position.incrementLine();
+			return makeHexicalNumberToken();
 		}
-		return c;
+
+		//binary number: 0b1010
+		else if (peekChar() == 'b')
+		{
+			return makeBinaryNumberToken();
+		}
+
+		//decimal number 0123, put back the '0' token we popped from the stream
+		pushChar(c);
 	}
-
-	cerror("could not open test.c");
-	return 'x';
+	return makeDecimalNumberToken();
 }
 
-void lexer::pushChar(char ch)
+std::shared_ptr < token > lexer::makeHexicalNumberToken()
 {
-	if (!m_file)
-		return;
-	m_file.putback(ch);
-	cerror("LEX error: could not open test.c");
+	_assert_(nextChar() == 'x');
+	unsigned long number = 0;
+	std::string number_str;
+	while (isHexChar(peekChar()))
+	{
+		number_str += peekChar();
+		nextChar();
+	}
+	number = strtol(number_str.c_str(), 0, 16);
+	return std::make_shared < token >(tokenType::TOKEN_TYPE_NUMBER, m_file_position, number);
 }
+
+std::shared_ptr < token > lexer::makeBinaryNumberToken()
+{
+	_assert_(nextChar() == 'b');
+	unsigned long number = 0;
+	std::string number_str;
+	while (peekChar() == '0' || peekChar() <= '1')
+	{
+		number_str += peekChar();
+		nextChar();
+	}
+	number = strtol(number_str.c_str(), 0, 2);
+	return std::make_shared < token >(tokenType::TOKEN_TYPE_NUMBER, m_file_position, number);
+}
+
+std::shared_ptr < token > lexer::makeDecimalNumberToken()
+{
+	unsigned long number = 0;
+	std::string number_str;
+	while (peekChar() >= '0' && peekChar() <= '9')
+	{
+		number_str += peekChar();
+		nextChar();
+	}
+	number = atoll(number_str.c_str());
+	return std::make_shared < token >(tokenType::TOKEN_TYPE_NUMBER, m_file_position, number);
+}
+
+bool lexer::isHexChar(char c)
+{
+	char c_lower = tolower(c);
+    return (c_lower >= '0' && c_lower <= '9') || (c_lower >= 'a' && c_lower <= 'f');
+}
+
--- compiler_lib/lexer.h
+++ compiler_lib/lexer.h
@@ -4,7 +4,10 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <list>
+
 #include "filePosition.h"
+#include "token.h"
 
 class lexer
 {
@@ -12,21 +15,35 @@ public:
 	lexer();
 	~lexer();
 
-	void initialize(std::string filename);
-	void startLexer();
-	void lexFile();
-
-
-private:
-
-	//TODO: these functions and ifstream should be provided by an interface(made by template?) 
-	//the lexer should not own the file stream
+//TODO: these functions and ifstream should be provided by an interface(made by template?) 
+//the lexer should not own the file stream
 	char peekChar();
 	char nextChar();
 	void pushChar(char ch);
 
+
+	void initialize(std::string filename);
+	void startLexer();
+	void lexFile();
+	void _assert_(bool condition);
+
+private:
+
+
+	std::shared_ptr < token > readNextToken();
+	std::shared_ptr < token > makeNumberToken();
+	std::shared_ptr < token > makeHexicalNumberToken();
+	std::shared_ptr < token > makeBinaryNumberToken();
+	std::shared_ptr < token > makeDecimalNumberToken();
+	bool isHexChar(char c);
+
+
+
 	std::string m_filename;
 	std::ifstream m_file;
 	filePosition m_file_position;
+
+	//list of tokens that has been lexed. Will be handed over to the parser
+	std::list < std::shared_ptr < token > > tokens;
 };
 
