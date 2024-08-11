19198c41db62f741f2e82eb5a5ae3c72f097619f
Ingvar August Wedervang <55133964+august95@users.noreply.github.com>
2024-08-11 22:29:31 +0200

    token class created

--- compiler_lib/token.cpp
+++ compiler_lib/token.cpp
@@ -2,6 +2,21 @@
 #include "token.h"
 
 token::token()
+	:m_token_tyoe(TOKEN_TYPE_NONE),
+	m_value(0)
 {
 
+}
+
+token::token(tokenType token_type, filePosition file_position, unsigned int value)
+	: m_token_tyoe(token_type),
+	m_file_position(file_position),
+	m_value(value)
+{
+
+}
+
+bool token::isTokenTypeNone()
+{
+	return m_token_tyoe == tokenType::TOKEN_TYPE_NONE;
 }
--- compiler_lib/token.h
+++ compiler_lib/token.h
@@ -1,9 +1,32 @@
 #pragma once
+#include "filePosition.h"
+
+
+enum tokenType
+{
+	TOKEN_TYPE_NONE,
+	TOKEN_TYPE_IDENTIFIER,
+	TOKEN_TYPE_KEYWORD,
+	TOKEN_TYPE_OPERATOR,
+	TOKEN_TYPE_SYMBOL,
+	TOKEN_TYPE_NUMBER,
+	TOKEN_TYPE_STRING,
+	TOKEN_TYPE_COMMENT,
+	TOKEN_TYPE_NEWLINE
+};
+
 class token
 {
 public:
 	token();
+	token(tokenType token_type, filePosition file_position, unsigned int value);
+	bool isTokenTypeNone();
 
+private:
+	tokenType m_token_tyoe;
+	filePosition m_file_position;
+	
+	unsigned long m_value;
 
 };
 
