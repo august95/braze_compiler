f9000d4a85ae811845a4ac09112b1e3d9b829fbb
Ingvar August Wedervang <55133964+august95@users.noreply.github.com>
2024-08-15 21:31:12 +0200

    token list added to parser, and function for nextToken and peekToken created

--- compiler_lib/parser.cpp
+++ compiler_lib/parser.cpp
@@ -1,7 +1,25 @@
 #include "pch.h"
 #include "parser.h"
 
+
 parser::parser()
 {
 
+}
+
+void parser::startParser()
+{
+	std::shared_ptr < token > token = nextToken();
+}
+
+std::shared_ptr < token > parser::nextToken()
+{
+	std::shared_ptr < token > token = m_tokens.front();
+	m_tokens.pop_front();
+	return token;
+}
+
+std::shared_ptr < token > parser::peekToken()
+{
+	return m_tokens.front();
 }
--- compiler_lib/parser.h
+++ compiler_lib/parser.h
@@ -1,7 +1,26 @@
 #pragma once
+
+//receives a list of tokens, creates an abstract syntax tree with nodes
+
+#include "token.h"
+#include <list>
+#include <iostream>
+
+
 class parser
 {
 public:
 	parser();
+
+	void setTokenList(std::list < std::shared_ptr < token > > tokens) { m_tokens = tokens; };
+	void startParser();
+
+private:
+
+	std::shared_ptr < token > nextToken();
+	std::shared_ptr < token > peekToken();
+
+private:
+	std::list < std::shared_ptr < token > > m_tokens;
 };
 
