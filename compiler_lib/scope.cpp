b67cacef8571421dc8e691f708871b60c873944c
Ingvar August Wedervang <ingvar.wedervang@gmail.com>
2025-02-05 19:51:54 +0100

    scope class added, used to calculate and keep stack size info

--- compiler_lib/scope.cpp
+++ compiler_lib/scope.cpp
@@ -1,7 +1,48 @@
 #include "pch.h"
 #include "scope.h"
+#include "braze_compiler.h"
 
 scope::scope()
+  :m_size(0),
+  m_flags(0),
+  m_is_root_scope(false)
 {
 
+}
+
+void scope::init(std::shared_ptr<scope> last_scope, bool is_root_scope)
+{
+  if (is_root_scope)
+  {
+    return;
+  }
+  m_parent = last_scope;
+}
+
+void scope::addNode(std::shared_ptr<node> node)
+{
+  std::shared_ptr < scopeEntity> scope_entity = std::make_shared < scopeEntity >();
+  scope_entity->setNode(node);
+  m_scope_entities.push_back(scope_entity);
+  m_size += scope_entity->getStackOffset();
+}
+
+void scope::updateStackOffset(std::shared_ptr<node> node, int stack_offset)
+{
+  for (auto it = m_scope_entities.begin(); it != m_scope_entities.end(); it++)
+  {
+    std::shared_ptr < scopeEntity> scope_entity = (*it);
+    if (node == scope_entity->getNode() && scope_entity->getNode())
+    {
+      m_size = stack_offset;
+      scope_entity->setStackOffset(stack_offset);
+      return;
+    }
+  }
+  if (!m_is_root_scope)
+  {
+    m_parent->updateStackOffset(node, stack_offset);
+    return;
+  }
+  cerror("Could not update stack offfset on node");
 }
--- compiler_lib/scope.h
+++ compiler_lib/scope.h
@@ -1,7 +1,23 @@
 #pragma once
+#include <iostream>
+#include <list>
+#include "scopeEntity.h"
+
+//maybe calculate all scope offsets after parsing in the future?
 class scope
 {
 public:
 	scope();
-};
 
+	void init(std::shared_ptr < scope > last_scope, bool is_root_scope = false);
+	void addNode(std::shared_ptr<node> node);
+	void updateStackOffset(std::shared_ptr<node> node, int stack_offset);
+	std::shared_ptr < scope > getParent() { return m_parent; }
+	bool isRootScope() { return m_is_root_scope; }
+private:
+	int m_flags;
+	size_t m_size;
+	bool m_is_root_scope;
+	std::list < std::shared_ptr < scopeEntity > > m_scope_entities;
+	std::shared_ptr < scope > m_parent;
+};
