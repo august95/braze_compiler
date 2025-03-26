a969e9b6fc1bc55f04b543af7231add9d48f8ca6
Ingvar August Wedervang <ingvar.wedervang@gmail.com>
2025-03-26 19:23:39 +0100

    started to implement codegeneration of global variables

--- compiler_lib/codeGenerator.cpp
+++ compiler_lib/codeGenerator.cpp
@@ -1,13 +1,29 @@
 #include "pch.h"
 #include "codeGenerator.h"
+#include "braze_compiler.h"
 
 codeGenerator::codeGenerator()
 {
 }
 
+void codeGenerator::initialize()
+{
+  int error = m_asm_writer.initialize(output_file);
+
+  if (error != 0)
+  {
+    cerror("failed to open file");
+  }
+}
+
+void codeGenerator::setFileName(std::string filename)
+{
+  m_input_file = filename;
+  output_file = filename + ".asm";
+}
+
 int codeGenerator::startCodeGeneration()
 {
-
   /*
 
   1. generate root scope
@@ -21,5 +37,60 @@ int codeGenerator::startCodeGeneration()
   5. generate read only data
 
   */
-  return -1;
+
+  initialize();
+
+  m_root_scope.init(std::make_shared<scope>(), true);
+
+  generateDataSection();
+
+
+  return 0;
+}
+
+void codeGenerator::generateDataSection()
+{
+  m_asm_writer.asmPush("section .data");
+
+  for (auto node : m_ast)
+  {
+    if (node->getNodeType() == NODE_TYPE_VARIABLE)
+    {
+      generateGlobalVariable(node);
+    }
+  }
+}
+
+void codeGenerator::generateGlobalVariable(std::shared_ptr < node > node)
+{
+  if (node)
+  {
+    if (node->getNodeType() == NODE_TYPE_VARIABLE)
+    {
+      std::shared_ptr < datatype > datatype = node->getDatatype();
+      
+      //do not deal with structs and unions here!
+      if (datatype->isPrimitiveType())
+      {
+        generateGlobalVariablePrimitive(node);
+      }
+    }
+  }
+}
+
+void codeGenerator::generateGlobalVariablePrimitive(std::shared_ptr<node> node)
+{
+  if (node->getNodeType() == NODE_TYPE_STRING)
+  {
+    cerror("Codegen: strings not yet implemented"); 
+    return;
+  }
+  std::string var_name = node->getStringValue();
+  std::string var_value = "0";
+  std::shared_ptr < datatype > datatype = node->getDatatype();
+  if (node->getValueNode())
+  {
+    var_value = std::to_string(node->getValueNode()->getNumberValue());
+  }
+  m_asm_writer.asmPush(var_name + " " + datatype->getStringForPrimitiveSize() + " " + var_value);
 }
--- compiler_lib/codeGenerator.h
+++ compiler_lib/codeGenerator.h
@@ -1,16 +1,27 @@
 #pragma once
 #include <list>
 #include "node.h"
+#include "scope.h"
+#include "asmWriter.h"
 
 class codeGenerator
 {
 public:
   codeGenerator();
   void setAbstractSyntaxTree(std::list < std::shared_ptr < node > > ast) { m_ast = ast; }
+  void initialize();
+  void setFileName(std::string filename);
   int startCodeGeneration();
+  void generateDataSection();
+  void generateGlobalVariable(std::shared_ptr < node > node);
+  void generateGlobalVariablePrimitive(std::shared_ptr < node > node);
 private:
 
+  scope m_root_scope;
+  std::string m_input_file;
+  std::string output_file;
   std::list < std::shared_ptr < node > > m_ast;
-  
+  asmWriter m_asm_writer;
+
 };
 
