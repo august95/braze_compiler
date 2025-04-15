03a1c5d600c35244699268c2769a9d4fe6db8779
Ingvar August Wedervang <55133964+august95@users.noreply.github.com>
2025-04-15 23:29:11 +0200

    precedence logic moved from ExpressionableOperatorPrecedence to precedenceHandler

--- compiler_lib/precedenceHandler.cpp
+++ compiler_lib/precedenceHandler.cpp
@@ -1,6 +1,31 @@
 #include "pch.h"
 #include "precedenceHandler.h"
-#include "ExpressionableOperatorPrecedence.h"
+#include <cstddef>
+
+// Assuming these are defined somewhere:
+#define TOTAL_OPERATOR_GROUPS 14
+#define ASSOSCIATIVITY_LEFT_TO_RIGHT 0
+#define ASSOSCIATIVITY_RIGHT_TO_LEFT 1
+
+
+
+expresssionable_op_precedence_group op_precedence[TOTAL_OPERATOR_GROUPS] = {
+	{{"++", "--", "()", "[]", "(", "[", ".", "->", NULL}, ASSOSCIATIVITY_LEFT_TO_RIGHT},
+	{{"*", "/", "%", NULL}, ASSOSCIATIVITY_LEFT_TO_RIGHT},
+	{{"+", "-", NULL}, ASSOSCIATIVITY_LEFT_TO_RIGHT},
+	{{"<<", ">>", NULL}, ASSOSCIATIVITY_LEFT_TO_RIGHT},
+	{{"<", ">", "<=", ">=", NULL}, ASSOSCIATIVITY_LEFT_TO_RIGHT},
+	{{"==", "!=", NULL}, ASSOSCIATIVITY_LEFT_TO_RIGHT},
+	{{"&", NULL}, ASSOSCIATIVITY_LEFT_TO_RIGHT},
+	{{"^", NULL}, ASSOSCIATIVITY_LEFT_TO_RIGHT},
+	{{"|", NULL}, ASSOSCIATIVITY_LEFT_TO_RIGHT},
+	{{"&&", NULL}, ASSOSCIATIVITY_LEFT_TO_RIGHT},
+	{{"||", NULL}, ASSOSCIATIVITY_LEFT_TO_RIGHT},
+	{{"?", ":", NULL}, ASSOSCIATIVITY_RIGHT_TO_LEFT},
+	{{"=", "+=", "-=", "*=", "/=", "%=", "<<=", ">>=", "&=", "^=", "|=", NULL}, ASSOSCIATIVITY_RIGHT_TO_LEFT},
+	{{",", NULL}, ASSOSCIATIVITY_LEFT_TO_RIGHT}
+};
+
 
 /*
 changing order according to precedence:
@@ -65,9 +90,53 @@ void precedenceHandler::reorderExpression(std::shared_ptr < node > node_)
 		node_->getRightNode() && node_->getRightNode()->getNodeType() == nodeType::NODE_TYPE_EXPRESSION)
 	{
 		//operator on the right side
-		if (precedence::leftOperatorHasPriority(node_->getStringValue(), node_->getRightNode()->getStringValue()))
+		if (precedenceHandler::leftOperatorHasPriority(node_->getStringValue().c_str(), node_->getRightNode()->getStringValue().c_str()))
 		{
 			shiftChildrenLeft(node_);
 		}
 	}
+}
+
+int precedenceHandler::getPrecendenceForOperator(const char* op, expresssionable_op_precedence_group** group_out)
+{
+	*group_out = nullptr;
+	for (int i = 0; i < TOTAL_OPERATOR_GROUPS; i++)
+	{
+		for (int b = 0; op_precedence[i].operators[b]; b++)
+		{
+			const char* _op = op_precedence[i].operators[b];
+			if (STRINGS_EQUAL(op, _op))
+			{
+				*group_out = &op_precedence[i];
+				return i;
+			}
+		}
+	}
+	return -1;
+}
+
+bool precedenceHandler::leftOperatorHasPriority(const char* op_left, const char* op_right)
+{
+	expresssionable_op_precedence_group* group_left = nullptr;
+	expresssionable_op_precedence_group* group_right = nullptr;
+
+	if (STRINGS_EQUAL(op_left, op_right))
+	{
+		return false;
+	}
+
+	int precedence_left = getPrecendenceForOperator(op_left, &group_left);
+	int precedence_right = getPrecendenceForOperator(op_right, &group_right);
+
+	if (precedence_left == -1 || precedence_right == -1)
+	{
+		return false; 
+	}
+
+	if (group_left->associtivity == ASSOSCIATIVITY_RIGHT_TO_LEFT)
+	{
+		return precedence_left < precedence_right;
+	}
+
+	return precedence_left <= precedence_right;
 }
--- compiler_lib/precedenceHandler.h
+++ compiler_lib/precedenceHandler.h
@@ -1,9 +1,16 @@
 #pragma once
 #include "node.h"
 
+struct expresssionable_op_precedence_group {
+	const char* operators[16]; // or whatever max size fits your needs
+	int associtivity;
+};
+
 class precedenceHandler
 {
 public:
 	static void shiftChildrenLeft(std::shared_ptr < node > node_);
 	static void reorderExpression(std::shared_ptr < node > node_);
+	static int getPrecendenceForOperator(const char* op, expresssionable_op_precedence_group** group_out);
+	static bool leftOperatorHasPriority(const char* op_left, const char* op_right);
 };
