116433c81ec5e91ef617d5ea1a6e3d4aeddd42c2
Ingvar August Wedervang <ingvar.wedervang@gmail.com>
2025-04-15 19:16:28 +0200

    vs files

--- compiler_lib/resolver.cpp
+++ compiler_lib/resolver.cpp
@@ -8,7 +8,5 @@ resolver::resolver()
 void resolver::createNewScope()
 
 {
-  std::shared_ptr <resolverScope> new_scope = std::make_shared <resolverScope>();
-  new_scope->init(m_next_scope);
-  m_next_scope = new_scope;
+
 }
