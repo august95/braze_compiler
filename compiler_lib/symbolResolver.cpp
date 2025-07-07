f01618025d53bf88f628009d567780440489d50c
Ingvar August Wedervang <55133964+august95@users.noreply.github.com>
2025-07-07 20:13:12 +0200

    added symbol resolver,  searches for declaration of identifier node

---
+++ compiler_lib/symbolResolver.cpp
@@ -0,0 +1,48 @@
+#include "pch.h"
+#include <assert.h>
+#include "symbolResolver.h"
+#include "braze_compiler.h"
+
+symbolResolver::symbolResolver()
+	:m_root_scope(std::make_shared< scope >())
+{
+	m_root_scope->init(std::shared_ptr<scope>(), true);
+	m_last_scope = m_root_scope;
+}
+
+void symbolResolver::finishScope()
+{
+	if (m_last_scope->isRootScope())
+	{
+		cerror("tried to delete root scope!");
+		assert(0);
+	}
+	m_last_scope = m_last_scope->getParent();
+}
+
+void symbolResolver::addNodeToCurrentScope(std::shared_ptr<node> node)
+{
+	m_last_scope->addNode(node);
+}
+
+void symbolResolver::newScope()
+{
+	std::shared_ptr<scope> _scope = std::make_shared<scope>();
+	_scope->init(m_last_scope);
+	m_last_scope = _scope;
+}
+
+std::shared_ptr<node> symbolResolver::findDeclerationNode(std::shared_ptr<node> identifier)
+{
+	return m_last_scope->find(identifier);
+}
+
+std::shared_ptr<node> symbolResolver::findVariableDeclerationNode(std::shared_ptr<node> identifier)
+{
+	return std::shared_ptr<node>();
+}
+
+std::shared_ptr<node> symbolResolver::findFunctionNode(std::shared_ptr<node> identifier)
+{
+	return std::shared_ptr<node>();
+}
---
+++ compiler_lib/symbolResolver.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "scope.h"
+
+class symbolResolver
+{
+public:
+  symbolResolver();
+  void finishScope();
+  void addNodeToCurrentScope(std::shared_ptr < node > node);
+  void newScope();
+  std::shared_ptr < node > findDeclerationNode(std::shared_ptr < node > identifier);
+private:
+  std::shared_ptr < node > findVariableDeclerationNode(std::shared_ptr < node > identifier);
+  std::shared_ptr < node > findFunctionNode(std::shared_ptr < node > identifier);
+private:
+  std::shared_ptr < scope > m_root_scope;
+  std::shared_ptr < scope > m_last_scope;
+};
+
